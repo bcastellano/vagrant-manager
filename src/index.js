dd5023e5252d2f556877fcbcc9e3f0ac4262430b
Blas Castellano Moreno <b.castellano.moreno@gmail.com>
2016-11-30 01:22:25 +0100

    Add menu and some actions: preferences, show, hide and reopen window, ...

--- src/index.js
+++ src/index.js
@@ -2,4 +2,23 @@
 
 import machines from './modules/machines'
 
-machines.show();
+/**
+ * App start
+ */
+class AppIndex {
+
+    start() {
+        this.registerEvents();
+
+        machines.show();
+    }
+
+    registerEvents() {
+        // listen main process calls
+        require('electron').ipcRenderer.on('open', (event, message) => {
+            require('./modules/settings').show()
+        });
+    }
+}
+
+(new AppIndex()).start();
--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ const debug = require('debug')('vagrant-manager:main');
 
 const {app,BrowserWindow} = require('electron');
 const path = require('path');
+const menu = require('./modules/menu')
 
-app.on('ready', ()=> {
-  debug('app ready');
-
-  if (process.platform=='darwin') {
-    //app.dock.hide() //Hide the icon in osx dock
-  }
-
+/**
+ * Open main window
+ */
+function openWindow() {
   let main_window = new BrowserWindow({ width: 1300, height: 1000 })
 
   // debug
@@ -20,15 +18,38 @@ app.on('ready', ()=> {
   main_window.on('close', function () { main_window = null });
 
   // load configuration
+  main_window.loadURL(`file://${__dirname}/main.html`);
+}
+
+/**
+ * on app ready
+ */
+app.on('ready', ()=> {
+  debug('app ready');
+
+  if (process.platform=='darwin') {
+    //app.dock.hide() //Hide the icon in osx dock
+  }
+
+  // load configuration
   let Configuration = require('./services/configuration');
   Configuration.on('initialized', ()=>{
     // show page when configuration has been loaded
-    main_window.loadURL(`file://${__dirname}/main.html`);
+    openWindow();
   })
 
 });
 
+/**
+ * on reopen window event
+ */
+app.on('reopenWindow', ()=> {
+  openWindow();
+});
 
+/**
+ * on main process exit
+ */
 process.on('exit', function (code) {
   debug('Process exited with code ' + code);
   console.log('Process exited with', code);
